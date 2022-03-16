740c0efec06ab6321b444e2211857d14126ef744
mrbaig97 <farazbaig521@gmail.com>
2022-03-16 20:05:12 +0500

    work done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import JitsiMeetComponent from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <JitsiMeetComponent/>
   </React.StrictMode>,
   document.getElementById('root')
 );
